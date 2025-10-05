32700db4f905e79df0cdea8b1157f63c90afc0cf
spokoynbIy <gusarov.tolia@mail.ru>
2025-10-05 22:36:05 +0500

    chore: обновить зависимости и разрешить запросы в интернет

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.navigation.compose)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

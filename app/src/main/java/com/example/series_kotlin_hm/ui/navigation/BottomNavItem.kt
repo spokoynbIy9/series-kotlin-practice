3c7264f04cf99557abf013b18ce5ab8fe37781c3
spokoynbIy <gusarov.tolia@mail.ru>
2025-10-15 19:52:46 +0500

    style: вынес в отдельный файл

---
+++ app/src/main/java/com/example/series_kotlin_hm/ui/navigation/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.example.series_kotlin_hm.ui.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+)

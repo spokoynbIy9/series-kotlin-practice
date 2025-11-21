176711a6ce4666c6257de247d583c6530e3feb76
spokoynbIy <gusarov.tolia@mail.ru>
2025-11-21 23:41:45 +0500

    feat: добавить поле favoriteClassTime в ProfileEntity

--- app/src/main/java/com/example/series_kotlin_hm/domain/model/ProfileEntity.kt
+++ app/src/main/java/com/example/series_kotlin_hm/domain/model/ProfileEntity.kt
@@ -3,6 +3,7 @@ package com.example.series_kotlin_hm.domain.model
 data class ProfileEntity(
     val fullName: String = "",
     val photoUri: String = "",
-    val resumeUrl: String = ""
+    val resumeUrl: String = "",
+    val favoriteClassTime: String = "" // Формат HH:mm
 )
 
